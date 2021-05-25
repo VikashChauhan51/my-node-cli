60f1e019c5da6f7f5596e36c00a49485e444758f
Vikash Chauhan <vikashchauhan51@gmail.com>
2021-05-25 09:48:17 +0530

    use arg instead of commander

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,35 @@
 #!/usr/bin/env node
 import * as chalk from "chalk";
 import * as figlet from "figlet";
-import { Command } from 'commander';
-const program = new Command();
+import * as arg from "arg";
+
+
+const args = arg(
+  {
+    '--path': String,
+    '--name': String,
+    '-p': '--path',
+    '-n': '--name',
+  },
+  {
+    argv: process.argv.slice(2),
+    stopAtPositional: false
+  }
+);
+
  export class Main {
     
- 
     /**
      * run
      */
     public run() {
-console.log(chalk.green(figlet.textSync('my-node-cli')));
-program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
-
-program.parse(process.argv);
-
-const options = program.opts();
-if (options.debug) console.log(options);
-console.log('pizza details:');
-if (options.small) console.log('- small pizza size');
-if (options.pizzaType) console.log(`- ${options.pizzaType}`);
+    let path = args["--path"] || args._[0];
+    let name = args["--name"] || args._[1];
+    console.log(chalk.green(figlet.textSync('my-node-cli')));
+    console.log(path);
+    console.log(name);
     }
+ 
 }
-//kickoff: entry point of an application.
-
+//kickoff: entry point of the application.
 new Main().run();
